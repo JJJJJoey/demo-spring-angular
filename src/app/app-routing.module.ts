e5df6e6e74ac5ba7637a70a646d1eea6edec4208
Georgia Saina <sainas.georgia@gmail.com>
2022-09-29 15:30:03 +0300

    store page creating

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { UserListComponent } from './user-list/user-list.component';
 import { UserFormComponent } from './user-form/user-form.component';
 import { StoreListComponent } from './store-list/store-list.component';
+import { StoreComponent } from './store/store.component';
 
 const routes: Routes = [
   { path: 'users', component: UserListComponent},
   { path: 'adduser', component: UserFormComponent},
-  { path: 'store-list', component: StoreListComponent}
+  { path: 'store-list', component: StoreListComponent},
+  { path: 'store', component: StoreComponent}
+
 
 ];
 
